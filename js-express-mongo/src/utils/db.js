319ff483a9573b411192dd81217a491504224eac
Kunal Kundu <kunal99kundu@gmail.com>
2022-02-11 14:11:09 +0530

    fix(js-express-mongo): enable validator on update requests for mongodb

--- js-express-mongo/src/utils/db.js
+++ js-express-mongo/src/utils/db.js
@@ -10,6 +10,9 @@ const connectToDb = async mongoUri => {
 	console.log(`mongoUri: ${mongoUri}`)
 	try {
 		await mongoose.connect(mongoUri, mongoOptions)
+		// Note: Enables validators on update requests
+		// To prevent for a update query use runValidators: false in opts
+		mongoose.set('runValidators', true)
 		console.log('connected to MongoDB')
 	} catch (err) {
 		console.log('error connecting to MongoDB:', err.message)

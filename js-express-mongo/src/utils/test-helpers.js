c5e1a882d9f84fa61a950b6b9fab4a0cb4a631a4
Kunal Kundu <kunal99kundu@gmail.com>
2022-02-11 14:16:13 +0530

    test(js-express-mongo): create utils to create user and get mock user data

--- js-express-mongo/src/utils/test-helpers.js
+++ js-express-mongo/src/utils/test-helpers.js
@@ -1,4 +1,7 @@
+const faker = require('faker') // eslint-disable-line node/no-unpublished-require
+
 const { User, Post, Like, Follow } = require('../models')
+const { createHash } = require('./auth')
 const { connectToDb } = require('./db')
 const { MONGODB_URI } = require('./config')
 
@@ -15,7 +18,29 @@ const mongoDbConnect = async () => {
 	await connectToDb(MONGODB_URI)
 }
 
+const getMockUserData = () => {
+	const user = {
+		username: faker.internet.userName(),
+		email: faker.internet.email(),
+		password: faker.internet.password(),
+	}
+	return user
+}
+
+const createUser = async () => {
+	const { username, email, password } = getMockUserData()
+	const passwordHash = createHash(password)
+	const user = await User.create({
+		username,
+		email,
+		passwordHash,
+	})
+	return user
+}
+
 module.exports = {
 	mongoDbCleanup,
 	mongoDbConnect,
+	getMockUserData,
+	createUser,
 }

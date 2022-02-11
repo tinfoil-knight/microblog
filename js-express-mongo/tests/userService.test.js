0fbce9355cfd1e5777ff2c80da7c450e8b650723
Kunal Kundu <kunal99kundu@gmail.com>
2022-02-12 02:02:01 +0530

    test(js-express-mongo): add test to check email update, auth token

--- js-express-mongo/tests/userService.test.js
+++ js-express-mongo/tests/userService.test.js
@@ -1,27 +1,44 @@
 const faker = require('faker')
 const test = require('ava')
+const jsonwebtoken = require('jsonwebtoken')
 const { UserService } = require('../src/services')
 const { User } = require('../src/models')
 
-const { mongoDbCleanup, mongoDbConnect } = require('../src/utils/test-helpers')
+const {
+	mongoDbCleanup,
+	mongoDbConnect,
+	getMockUserData,
+	createUser,
+} = require('../src/utils/test-helpers')
 
 test.before(async () => {
 	await mongoDbConnect()
 })
 
-test.after.always('mongodb cleanup', () => {
-	mongoDbCleanup()
+test.after.always('mongodb cleanup', async () => {
+	await mongoDbCleanup()
 })
 
 test('should sign-up user', async t => {
-	const username = faker.internet.userName()
-	const user = {
-		username,
-		email: faker.internet.email(),
-		password: faker.internet.password(),
-	}
+	const user = getMockUserData()
 	await UserService.SignUp(user)
-	const result = await User.findOne({ username }).lean()
+	const result = await User.findOne({ username: user.username }).lean()
 	t.truthy(result)
 	t.pass()
+})
+
+test('can update user email', async t => {
+	const user = await createUser()
+	const newMail = faker.internet.email()
+	await UserService.UpdateMail(user._id, newMail)
+	const result = await User.findById(user._id)
+	t.is(result.email, newMail)
+})
+
+test('can get auth token with correct password', async t => {
+	const userData = getMockUserData()
+	const user = await createUser(userData)
+	const token = await UserService.GetToken(user.username, userData.password)
+	const decoded = jsonwebtoken.verify(token, process.env.TOKEN_SECRET)
+	t.is(decoded.id, String(user._id))
 })

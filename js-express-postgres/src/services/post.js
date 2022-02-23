110c5f77549b27b83a468b00d632d58f67c899ee
Kunal Kundu <kunal99kundu@gmail.com>
2022-02-24 01:27:03 +0530

    refactor(js-express-postgres): replace mongoose queries by prisma in PostService for some methods

--- js-express-postgres/src/services/post.js
+++ js-express-postgres/src/services/post.js
@@ -1,21 +1,31 @@
+const { PrismaClient } = require('@prisma/client')
+
 const HttpError = require('../utils/error')
-const { addJob } = require('../utils/queue')
+// const { addJob } = require('../utils/queue')
+
+const prisma = new PrismaClient()
 
 module.exports = class PostService {
-	static async CreatePost({ content, author }) {
-		const post = new Post({ content, author })
-		await post.save()
-		addJob('fanout', { authorId: author, postId: post._id })
+	static async CreatePost({ content, authorId }) {
+		const post = await prisma.post.create({ data: { content, authorId } })
+		// addJob('fanout', { authorId, postId: post.id })
+		return post.id
 	}
 
 	static async GetPost(postId) {
-		const [post, likes] = await Promise.all([
-			Post.findById(postId)
-				.populate({ path: 'author', select: 'username -_id' })
-				.select('content createdAt -_id')
-				.lean(),
-			Like.countDocuments({ post: postId }),
-		])
+		const post = await prisma.post.findUnique({
+			where: { id: postId },
+			select: {
+				author: {
+					select: { username: true },
+				},
+				content: true,
+				createdAt: true,
+				_count: {
+					select: { likes: true },
+				},
+			},
+		})
 		if (!post) {
 			throw new HttpError(404)
 		}
@@ -23,17 +33,23 @@ module.exports = class PostService {
 			author: post.author.username,
 			content: post.content,
 			createdAt: post.createdAt,
-			likes,
+			likes: post._count.likes,
 		}
 	}
 
-	static async Like(postId, userId) {
-		const like = new Like({ post: postId, user: userId })
-		await like.save()
+	static Like(postId, userId) {
+		return prisma.like.create({ data: { postId, userId } })
 	}
 
-	static async Unlike(postId, userId) {
-		await Like.deleteOne({ post: postId, user: userId })
+	static Unlike(postId, userId) {
+		return prisma.like.delete({
+			where: {
+				postId_userId: {
+					postId,
+					userId,
+				},
+			},
+		})
 	}
 
 	static async GetLikes(postId, cursor) {
@@ -58,17 +74,14 @@ module.exports = class PostService {
 	}
 
 	static async DeletePost(postId, userId) {
-		const post = await Post.findById(postId).select('author -_id').lean()
-		if (!post) {
-			throw new HttpError(404)
-		}
-		const authorId = String(post.author)
-		if (userId !== authorId) {
+		const post = await prisma.post.findUnique({
+			where: { id: postId },
+			select: { authorId: true },
+		})
+		if (userId !== post.authorId) {
 			throw new HttpError(403)
 		}
-		await Promise.all([
-			Like.deleteMany({ post: postId }),
-			Post.deleteOne({ _id: postId }),
-		])
+		await prisma.like.deleteMany({ where: { postId } })
+		await prisma.post.delete({ where: { id: postId } })
 	}
 }

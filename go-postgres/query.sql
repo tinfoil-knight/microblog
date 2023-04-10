85f36cec11fb7683c96e02ca1bd9e563ab57e0c6
Kunal Kundu <kunal99kundu@gmail.com>
2023-04-11 00:37:33 +0530

    go-postgres: add queries to add,get,del,like,unlike posts

--- go-postgres/query.sql
+++ go-postgres/query.sql
@@ -26,3 +26,44 @@ SELECT
 FROM
     posts;
 
+-- name: InsertPost :exec
+INSERT INTO posts(content, author_id)
+    VALUES ($1, $2);
+
+-- name: DeletePost :exec
+DELETE FROM posts
+WHERE id = $1;
+
+-- name: GetPost :one
+SELECT
+    p.content,
+    p.author_id,
+    p.created_at,
+(
+        SELECT
+            username
+        FROM
+            users u
+        WHERE
+            u.id = p.author_id) AS username,
+(
+        SELECT
+            count(*)
+        FROM
+            likes l
+        WHERE
+            l.post_id = p.id) AS like_count
+FROM
+    posts p
+WHERE
+    p.id = $1;
+
+-- name: LikePost :exec
+INSERT INTO likes(post_id, user_id)
+    VALUES ($1, $2);
+
+-- name: UnlikePost :exec
+DELETE FROM likes
+WHERE post_id = $1
+    AND user_id = $2;
+
--- go-postgres/sqlc/query.sql.go
+++ go-postgres/sqlc/query.sql.go
@@ -7,7 +7,19 @@ package sqlc
 
 import (
 	"context"
+
+	"github.com/jackc/pgx/v5/pgtype"
 )
+
+const deletePost = `-- name: DeletePost :exec
+DELETE FROM posts
+WHERE id = $1
+`
+
+func (q *Queries) DeletePost(ctx context.Context, id int32) error {
+	_, err := q.db.Exec(ctx, deletePost, id)
+	return err
+}
 
 const getAllPostIDs = `-- name: GetAllPostIDs :many
 SELECT
@@ -63,6 +75,52 @@ func (q *Queries) GetAllUserIDs(ctx context.Context) ([]int32, error) {
 	return items, nil
 }
 
+const getPost = `-- name: GetPost :one
+SELECT
+    p.content,
+    p.author_id,
+    p.created_at,
+(
+        SELECT
+            username
+        FROM
+            users u
+        WHERE
+            u.id = p.author_id) AS username,
+(
+        SELECT
+            count(*)
+        FROM
+            likes l
+        WHERE
+            l.post_id = p.id) AS like_count
+FROM
+    posts p
+WHERE
+    p.id = $1
+`
+
+type GetPostRow struct {
+	Content   string             `json:"content"`
+	AuthorID  int32              `json:"author_id"`
+	CreatedAt pgtype.Timestamptz `json:"created_at"`
+	Username  string             `json:"username"`
+	LikeCount int64              `json:"like_count"`
+}
+
+func (q *Queries) GetPost(ctx context.Context, id int32) (GetPostRow, error) {
+	row := q.db.QueryRow(ctx, getPost, id)
+	var i GetPostRow
+	err := row.Scan(
+		&i.Content,
+		&i.AuthorID,
+		&i.CreatedAt,
+		&i.Username,
+		&i.LikeCount,
+	)
+	return i, err
+}
+
 type InsertFollowsParams struct {
 	FollowerID  int32 `json:"follower_id"`
 	FollowingID int32 `json:"following_id"`
@@ -71,6 +129,21 @@ type InsertFollowsParams struct {
 type InsertLikesParams struct {
 	PostID int32 `json:"post_id"`
 	UserID int32 `json:"user_id"`
+}
+
+const insertPost = `-- name: InsertPost :exec
+INSERT INTO posts(content, author_id)
+    VALUES ($1, $2)
+`
+
+type InsertPostParams struct {
+	Content  string `json:"content"`
+	AuthorID int32  `json:"author_id"`
+}
+
+func (q *Queries) InsertPost(ctx context.Context, arg InsertPostParams) error {
+	_, err := q.db.Exec(ctx, insertPost, arg.Content, arg.AuthorID)
+	return err
 }
 
 type InsertPostsParams struct {
@@ -82,4 +155,35 @@ type InsertUsersParams struct {
 	Email        string `json:"email"`
 	Username     string `json:"username"`
 	PasswordHash string `json:"password_hash"`
+}
+
+const likePost = `-- name: LikePost :exec
+INSERT INTO likes(post_id, user_id)
+    VALUES ($1, $2)
+`
+
+type LikePostParams struct {
+	PostID int32 `json:"post_id"`
+	UserID int32 `json:"user_id"`
+}
+
+func (q *Queries) LikePost(ctx context.Context, arg LikePostParams) error {
+	_, err := q.db.Exec(ctx, likePost, arg.PostID, arg.UserID)
+	return err
+}
+
+const unlikePost = `-- name: UnlikePost :exec
+DELETE FROM likes
+WHERE post_id = $1
+    AND user_id = $2
+`
+
+type UnlikePostParams struct {
+	PostID int32 `json:"post_id"`
+	UserID int32 `json:"user_id"`
+}
+
+func (q *Queries) UnlikePost(ctx context.Context, arg UnlikePostParams) error {
+	_, err := q.db.Exec(ctx, unlikePost, arg.PostID, arg.UserID)
+	return err
 }
